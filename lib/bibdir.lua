05eaeb7785b844992f8d93978cc1c8213e4e523d
ocornoc <ocornoc@protonmail.com>
2018-08-17 19:28:19 -0700

    Added initial directory creation script

--- lib/bibdir.lua
+++ lib/bibdir.lua
@@ -1,6 +1,7 @@
 local lfs = require "lfs"
 local files = {}
 
+<<<<<<< HEAD
 files.home = os.getenv("HOME")
 
 files.main_folder_path = files.home .. "/.bibliosoph"
@@ -16,3 +17,17 @@ lfs.mkdir(files.logs_folder_path)
 files.log_file_path = files.logs_folder_path .. "/bibliolog.txt"
 
 return files
+=======
+lfs.mkdir("~/.bibliosoph")
+files.main_folder_path = "~/.bibliosoph"
+
+lfs.chdir(files.folder_path)
+
+lfs.mkdir("./keys")
+files.key_folder_path = files.main_folder_path .. "/keys"
+files.key_file_path = files.key_folder_path .. "/keyfile"
+
+lfs.mkdir("./logs")
+files.logs_folder_path = files.main_folder_path .. "/logs"
+files.log_file_path = files.logs_folder_path .. "/" .. os.date("!%t")
+>>>>>>> Added initial directory creation script
