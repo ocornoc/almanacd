35ece4d347c3a1bab3b670ea359b3550fa02ebd7
ocornoc <ocornoc@protonmail.com>
2018-08-17 19:28:19 -0700

    Added initial directory creation script

---
+++ lib/bibdir.lua
@@ -0,0 +1,15 @@
+local lfs = require "lfs"
+local files = {}
+
+lfs.mkdir("~/.bibliosoph")
+files.main_folder_path = "~/.bibliosoph"
+
+lfs.chdir(files.folder_path)
+
+lfs.mkdir("./keys")
+files.key_folder_path = files.main_folder_path .. "/keys"
+files.key_file_path = files.key_folder_path .. "/keyfile"
+
+lfs.mkdir("./logs")
+files.logs_folder_path = files.main_folder_path .. "/logs"
+files.log_file_path = files.logs_folder_path .. "/" .. os.date("!%t")
